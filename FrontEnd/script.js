b82b4919966e91a27b96d7b0b9fc225d2438e1e9
Joseph Baja <jbaja@student.neumont.edu>
2019-10-04 16:42:49 -0600

    errors for log in and sign up pages

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -25,9 +25,87 @@ app.controller('searchController', function ($scope, $http, $log, $window, $rout
 });
 
 app.controller('loginController', function ($scope, $http, $log, $window, $routeParams) {
+    $scope.username = null;
+    $scope.password = null;
+    $scope.logIn = function () {
+        $log.info($scope.username);
+        $log.info($scope.password);
+        document.getElementById('userInput').style.border = '2px solid #000';
+        document.getElementById('passInput').style.border = '2px solid #000';
+        document.getElementById('error').innerHTML = "";
+        let un = $scope.username;
+        let pw = $scope.password;
+        let unTrim;
+        let pwTrim;
 
+        if (un != null) {
+            unTrim = un.trim();
+        }
+
+        if (pw != null) {
+            pwTrim = pw.trim();
+        }
+
+        console.log(pw);
+        console.log(un);
+        console.log(pwTrim);
+        console.log(unTrim);
+
+        if ((un == null && pw != null) || (unTrim == "" && pwTrim != "")) {
+            document.getElementById("error").innerHTML = "Please enter in an username.";
+            document.getElementById('userInput').style.border = '2px solid #FF0000';
+        }
+        else if ((pw == null && un != null) || (pwTrim == "" && unTrim != "")) {
+            document.getElementById("error").innerHTML = "Please enter in a password.";
+            document.getElementById('passInput').style.border = '2px solid #FF0000';
+        }
+        else if ((un == null || unTrim == "") && (pw == null || pwTrim == "")) {
+            document.getElementById("error").innerHTML = "Please enter in an username and password.";
+            document.getElementById('userInput').style.border = '2px solid #FF0000';
+            document.getElementById('passInput').style.border = '2px solid #FF0000';
+        }
+    }
 });
 
 app.controller('signupController', function ($scope, $http, $log, $window, $routeParams) {
+    $scope.username = null;
+    $scope.password = null;
+    $scope.signUp = function () {
+        $log.info($scope.username);
+        $log.info($scope.password);
+        document.getElementById('userInput').style.border = '2px solid #000';
+        document.getElementById('passInput').style.border = '2px solid #000';
+        document.getElementById('error').innerHTML = "";
+        let un = $scope.username;
+        let pw = $scope.password;
+        let unTrim;
+        let pwTrim;
 
+        if (un != null) {
+            unTrim = un.trim();
+        }
+
+        if (pw != null) {
+            pwTrim = pw.trim();
+        }
+
+        console.log(pw);
+        console.log(un);
+        console.log(pwTrim);
+        console.log(unTrim);
+
+        if ((un == null && pw != null) || (unTrim == "" && pwTrim != "")) {
+            document.getElementById("error").innerHTML = "Please enter in an username.";
+            document.getElementById('userInput').style.border = '2px solid #FF0000';
+        }
+        else if ((pw == null && un != null) || (pwTrim == "" && unTrim != "")) {
+            document.getElementById("error").innerHTML = "Please enter in a password.";
+            document.getElementById('passInput').style.border = '2px solid #FF0000';
+        }
+        else if ((un == null || unTrim == "") && (pw == null || pwTrim == "")) {
+            document.getElementById("error").innerHTML = "Please enter in an username and password.";
+            document.getElementById('userInput').style.border = '2px solid #FF0000';
+            document.getElementById('passInput').style.border = '2px solid #FF0000';
+        }
+    }
 });
